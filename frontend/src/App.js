38a8a838c5648ba198b9fb9b4eea462f4f20d63c
Shivam Mishra <shivammishra7763914693@gmail.com>
2020-07-29 23:21:28 +0530

    completed_sing_registertion_from

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,13 @@ import { BrowserRouter, Route,Link } from "react-router-dom";
 import HomeScreen from './screen/HomeScreen';
 import ProductScreen from './screen/ProductScreen';
 import CartScreen from './screen/CartScreen';
+import SigninScreen from './screen/SigninScreen';
+import { useSelector } from 'react-redux';
+import RegisterScreen from './screen/RegisterScreen';
 function App() {
+    const userSignin = useSelector(state => state.userSignin);
+    const { userInfo } = userSignin;
+  
     const openMenu =()=>{
         document.querySelector('.sidebar').classList.add('open');
     }
@@ -24,7 +30,10 @@ function App() {
             </div>
             <div className='header-links'>
                 <a href='cart.html'>Cart</a>
-                <a href='singnin.html'>Sing In</a>
+                {
+              userInfo ? <Link to="/profile">{userInfo.name}</Link> :
+                <Link to="/signin">Sign In</Link>
+            }
             </div>
             </header>
             <aside className='sidebar'>
@@ -44,6 +53,8 @@ function App() {
       <main className='main'>
                    <div className='content'>
                    <Route path='/product/:id'  component={ProductScreen} />
+                   <Route path="/signin" component={SigninScreen} />
+                   <Route path="/register" component={RegisterScreen} />
                    <Route path="/cart/:id?" component={CartScreen} />
                    <Route path='/' exact={true} component={HomeScreen} />
                    </div>
