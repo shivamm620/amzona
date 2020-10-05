e6ca181fadb9cad1d73486ebb34c4edbb75f9b4b
Shivam Mishra <51381033+shivamm620@users.noreply.github.com>
2020-10-05 22:40:45 +0530

    register routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,7 +4,38 @@ import db from '../db';
 import {getToken} from '../util'
 
 
-
+router.post("/register",async(req,res)=>{
+    const {email, name , password} = req.body;
+    const hash =  bcrypt.hashSync(password);
+    db.transaction(trx =>{
+        trx.insert({
+            password: hash,
+            email : email,
+            isadmin:'false'
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail =>{
+            return trx ('users')
+            .returning('*')
+    .insert({
+        email:loginEmail[0],
+        name:name,
+        joined: new Date()
+    }).then(user =>{
+        res.json({
+            name: user[0].name,
+            id : user[0].id,
+            email : user[0].email,
+            getToken:getToken(user[0])
+        })
+    })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json('unable to register')) 
+})
 router.get('/createadmin' , async (req, res)=>{
     db.transaction(trx =>{
         trx.insert({
