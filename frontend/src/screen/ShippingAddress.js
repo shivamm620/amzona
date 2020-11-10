b4d1991e290a48c9bb1a5e294abf5b0ee77f9816
Shivam Mishra <shivammishra7763914693@gmail.com>
2020-11-10 22:29:47 +0530

    checkOut_steps

---
+++ frontend/src/screen/ShippingAddress.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Checkout from './Checkoutstep';
+
+export default function ShippingAddress (props){
+    return(
+        <div>
+    <Checkout step1 step2></Checkout>
+    </div>
+    )
+}
+
