f06f2a7f23c8e30635ef2951f15901924ad17b15
Shivam Mishra <shivammishra7763914693@gmail.com>
2020-11-18 21:46:49 +0530

    loading_and_messageBox

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Link } from "react-router-dom";
+import Rating from './Rating'
+export default function Product (props){
+    const { product } = props
+    return (
+        <ul className="products">
+        <li key={product._id}>
+        <div className='product'>
+        <Link to={`/product/`+product.name}>
+        <img className="products-image" src={product.image} alt='product'/>
+        </Link>
+        
+            <div className='product-name'>
+                <Link to={`/product/`+ product.name}>{product.name}</Link>
+            </div>
+            <div className='product-brand'>{product.brand}</div>
+            <Rating
+          rating={product.rating}
+          numReviews={product.numReviews}
+        ></Rating>
+            <div className='product-price'>${product.price}</div>
+            
+        </div>
+    </li>
+    </ul>
+    )
+}
